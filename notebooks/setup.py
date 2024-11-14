ecd4dc22838caceaea3a89f1811a807f618610bf
MendyData <mendy.data@gmail.com>
2024-11-14 21:16:47 +0100

    Ajout du fichier .gitignore

---
+++ notebooks/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Projet Smart City avec analyse des données urbaines',
+    author='Angele GOMIS',
+    license='',
+)
